e135773459966b89582af1e9ce8bbb29358132a5
Alvin Balmeo <alvinbalmeo@Alvins-MacBook-Pro.local>
2018-11-29 00:25:55 -0800

    initial commit

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; 
+
+const UserSchema = new Schema({
+    name: {
+        type: String, 
+        required: true
+    }, 
+    email: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: true
+    },
+    date: {
+        type: Date, 
+        default: Date.now
+    }
+
+
+})
+
+UserSchema.virtual('profile', {
+    ref: 'profile', 
+    localField: 'user',
+    foreighnFielld: "id",
+    justOne: true
+})
+
+module.exports = User = mongoose.model('users', UserSchema);
+

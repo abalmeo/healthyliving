45c786449b7fb861d25431eea0b9ad8f815db4a0
Alvin Balmeo <alvinbalmeo@Alvins-MacBook-Pro.local>
2018-11-29 00:29:10 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const bodyParser = require('body-parser');
+const passport = require('passport'); 
+const path = require('path'); 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+
+const app = express(); 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
+
+//Passport middleware 
+app.use(passport.initialize())
+
+//Passport Config JWT Strategy 
+require('./config/passport')(passport); 
+
+
+//Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+
+//Serve static assets if in production
+
+if(process.env.NODE_ENV ==='production'){
+    //Set static folder to client build
+    app.use(express.static('client/build')); 
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html')); 
+    })
+}
+
+
+
+const port = process.env.PORT || 8000;
+
+//Start Server
+app.listen(port, () => console.log(`Serving running on port ${port}`));
---
+++ validation/profile.js
@@ -0,0 +1,33 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+  console.log(data); 
+
+  data.bloodPressure = !isEmpty(data.bloodPressure) ? data.bloodPressure : '';
+  data.weight = !isEmpty(data.weight) ? data.weight : '';
+  data.bloodSugar = !isEmpty(data.bloodSugar) ? data.bloodSugar : '';
+
+  if (!Validator.isLength(data.userName, { min: 2, max: 40 })) {
+        errors.userName = 'Username should be between 2 and 4 characters long';
+  }
+
+  if (Validator.isEmpty(data.bloodPressure)) {
+    errors.bloodPressure = 'Blood pressure is required';
+  }
+
+  if (Validator.isEmpty(data.weight)) {
+    errors.weight = 'Weight is required';
+  }
+
+  if (Validator.isEmpty(data.bloodSugar)) {
+    errors.bloodSugar = 'Blood sugar are required';
+  }
+
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};

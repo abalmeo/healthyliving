630e24a5917e286085815cbdd413531b4acf8605
Alvin Balmeo <alvinbalmeo@Alvins-MacBook-Pro.local>
2018-12-02 23:37:56 -0800

    updating health form inputs with BW, Blood Glucose, and Blood Pressure

--- client/src/components/pages/HealthForms.js
+++ client/src/components/pages/HealthForms.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import { Redirect } from "react-router-dom";
+import DateTime from "../../components/pages/DatePicker";
 
 
 
@@ -40,11 +41,36 @@ class HealthForms extends Component {
     return (
       <div className="healthforms">
         <div className="container">
+        <DateTime/>
+
+        <form>
+ 
+        <div class="form-group row">
+            <label for="inputBodyWeight" class="col-sm-12 col-form-label">Body Weight</label>
+            <div class="col-sm-12">
+            <input type="number" class="form-control" id="inputBodyWeight" placeholder="Body Weight (lbs)"></input>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputBloodGlucose" class="col-sm-12 col-form-label">Blood Glucose</label>
+            <div class="col-sm-12">
+            <input type="number" class="form-control" id="inputBloodGlucose" placeholder="Blood Glucose (mg/dL)"></input>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputBloodPressure" class="col-sm-12 col-form-label">Blood Pressure</label>
+            <div class="col-sm-6">
+                <label for="inputBloodPressure" class="col-sm-6 col-form-label">Systolic</label>
+                <input type="number" class="form-control" id="inputBloodPressure" placeholder="mm Hg(higher)"></input>
+            </div>
+            <div class="col-sm-6">
+            <label for="inputBloodPressure" class="col-sm-6 col-form-label">Diastolic</label>
+            <input type="number" class="form-control" id="inputBloodPressure" placeholder="mm Hg(lower)"></input>
+            </div>
+        </div>
+        </form>
     
-                <p>        
-                  Input your form information
-                </p>
-          </div>
+        </div>
       </div>
     );
   }

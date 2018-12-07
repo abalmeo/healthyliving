dc4e6c0539e50abd3c73761de05963ae8f877fe1
Alvin Balmeo <alvinbalmeo@Alvins-MacBook-Pro.local>
2018-12-07 10:43:10 -0800

    styling graphs

--- client/src/components/graphs/BloodGlucose.js
+++ client/src/components/graphs/BloodGlucose.js
@@ -1,43 +1,53 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import {Bar, Line, Pie} from "react-chartjs-2";
+import { Bar, Line, Pie } from "react-chartjs-2";
 import "chartjs-plugin-annotation";
 
 
 class BloodGlucoseChart extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            chartDataBW:{
-                labels: ['12/1/2018','12/2/2018','12/3/2018',
-                '12/4/2018','12/5/2018','12/6/2018','12/7/2018'
+        this.state = {
+            chartDataBG: {
+                labels: ['12/1/2018', '12/2/2018', '12/3/2018',
+                    '12/4/2018', '12/5/2018', '12/6/2018', '12/7/2018'
                 ],
                 datasets: [
                     {
                         label: 'Blood Glucose',
-                        data:[
+                        data: [
                             160, 154, 152, 152, 153, 163, 154
                         ],
                         backgroundColor: [
                             'rgba(255,99,132,0.6'
                         ]
-                    
+
                     }
                 ]
             }
         }
-        }
-    render(){       
-      return(
-            <div className="chart">
-             <Line
-	         data={this.state.chartDataBW}
-	         options={{
-                maintainAspectRatio: false
-             }}
-             />
-        </div>
-       )
-     }
     }
+    render() {
+        return (
+            <div className="chart">
+                <Line
+                    data={this.state.chartDataBG}
+                    options={{
+                        maintainAspectRatio: false,
+                        title: {
+                            display: true,
+                            text: "Blood Glucose (mg/dL)",
+                            fontSize: 15
+                        },
+                        legend: {
+                            display: false,
+                            position: 'right',
+                            title: 'Blood Glucose'
+                        }
+                    }}
+                />
+            </div>
+        )
+    }
+}
 export default BloodGlucoseChart;

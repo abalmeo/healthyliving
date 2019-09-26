c205f02e918a165d052f0d52979e5b0e17030067
Alvin Balmeo <albalmeo@gmail.com>
2019-09-26 01:50:14 -0700

    changing Graph to useState

--- client/src/components/pages/Graphs.js
+++ client/src/components/pages/Graphs.js
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
-import axios from "axios";
-import {Bar, Line, Pie} from "react-chartjs-2";
-import "chartjs-plugin-annotation";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Bar, Line, Pie } from 'react-chartjs-2';
+import 'chartjs-plugin-annotation';
 
-
-class Chart extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-            chartDataBW:{
-                labels: ['12/1/2018','12/2/2018','12/3/2018',
-                '12/4/2018','12/5/2018','12/6/2018','12/7/2018'
-                ],
-                datasets: [
-                    {
-                        label: 'Body Weight',
-                        data:[
-                            160, 154, 152, 152, 153, 163, 154
-                        ],
-                        backgroundColor: [
-                            'rgba(255,99,132,0.6'
-                        ]
-                    
-                    }
-                ]
-            }
+const Chart = () => {
+  const [chartData, setChartData] = useState({
+    chartDataBW: {
+      labels: [
+        '12/1/2018',
+        '12/2/2018',
+        '12/3/2018',
+        '12/4/2018',
+        '12/5/2018',
+        '12/6/2018',
+        '12/7/2018'
+      ],
+      datasets: [
+        {
+          label: 'Body Weight',
+          data: [160, 154, 152, 152, 153, 163, 154],
+          backgroundColor: ['rgba(255,99,132,0.6']
         }
-        }
-    render(){       
-      return(
-            <div className="chart">
-             <Line
-	         data={this.state.chartDataBW}
-	         options={{
-                maintainAspectRatio: false
-             }}
-             />
-        </div>
-       )
-     }
+      ]
     }
+  });
+
+  return (
+    <div className="chart">
+      <Line
+        data={chartData}
+        options={{
+          maintainAspectRatio: false
+        }}
+      />
+    </div>
+  );
+};
 export default Chart;

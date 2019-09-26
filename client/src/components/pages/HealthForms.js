03f34a8c3a12d6c2aeac10c353c06d2e2124dcc8
Alvin Balmeo <albalmeo@gmail.com>
2019-09-26 02:05:17 -0700

    Changing Healthforms to use functional components

--- client/src/components/pages/HealthForms.js
+++ client/src/components/pages/HealthForms.js
@@ -1,82 +1,134 @@
-import React, { Component } from 'react';
-import axios from "axios";
-import { Redirect } from "react-router-dom";
-import DateTime from "../../components/pages/DatePicker";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+import DateTime from '../../components/pages/DatePicker';
 
+const HealthForms = () => {
+  const [healthData, setHealthData] = useState({
+    name: '',
+    bodyWeight: '',
+    bloodGlucose: '',
+    bloodPressure: '',
+    systolic: '',
+    diastolic: '',
+    dateTime: '',
+    redirectTo: null
+  });
+  const {
+    name,
+    bodyWeight,
+    bloodGlucose,
+    bloodPressure,
+    systolic,
+    diastolic,
+    dateTime,
+    redirectTo
+  } = healthData;
 
+  const onFormChange = e => {
+    setHealthData({ ...healthData, [e.target.name]: e.target.value });
+  };
 
-class HealthForms extends Component {
-  constructor(){
-      super();
-          this.state = {
-                name:"",
-                bodyWeight:"",
-                bloodGlucose:"",
-                bloodPressure:"",
-                dateTime:"",
-                redirectTo:null
-              }
-              
-              this.onChange = this.onChange.bind(this);
-              this.onSubmit = this.onSubmit.bind(this);
-        }
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const data = {
+      bodyWeight: bodyWeight,
+      bloodGlucose: bloodGlucose,
+      bloodPressure: bloodPressure,
+      dateTime: dateTime
+    };
+    console.log('data', data);
+  };
 
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+  return (
+    <div className="healthforms">
+      <div className="container">
+        <DateTime />
 
-    onSubmit(e) {
-        e.preventDefault();
-        const data = {
-            bodyWeight: this.state.bodyWeight,
-            bloodGlucose: this.state.bloodGlucose,
-            bloodPressure: this.state.bloodPressure,
-            dateTime: this.state.dateTime
-    
-        }
-    }
-      
-  
-  
-  
-    render() {
-    return (
-      <div className="healthforms">
-        <div className="container">
-        <DateTime/>
-
-        <form>
- 
-        <div className="form-group row">
-            <label htmlFor="inputBodyWeight" className="col-sm-12 col-form-label">Body Weight</label>
+        <form onSubmit={e => onFormSubmit(e)}>
+          <div className="form-group row">
+            <label
+              htmlFor="inputBodyWeight"
+              className="col-sm-12 col-form-label"
+            >
+              Body Weight
+            </label>
             <div className="col-sm-12">
-            <input type="number" className="form-control" id="inputBodyWeight" placeholder="Body Weight (lbs)"></input>
+              <input
+                type="number"
+                className="form-control"
+                id="inputBodyWeight"
+                placeholder="Body Weight (lbs)"
+                value={bodyWeight}
+                onChange={e => onFormChange(e)}
+              ></input>
             </div>
-        </div>
-        <div className="form-group row">
-            <label htmlFor="inputBloodGlucose" className="col-sm-12 col-form-label">Blood Glucose</label>
+          </div>
+          <div className="form-group row">
+            <label
+              htmlFor="inputBloodGlucose"
+              className="col-sm-12 col-form-label"
+            >
+              Blood Glucose
+            </label>
             <div className="col-sm-12">
-            <input type="number" className="form-control" id="inputBloodGlucose" placeholder="Blood Glucose (mg/dL)"></input>
+              <input
+                type="number"
+                className="form-control"
+                id="inputBloodGlucose"
+                placeholder="Blood Glucose (mg/dL)"
+                value={bloodGlucose}
+                onChange={e => onFormChange(e)}
+              ></input>
             </div>
-        </div>
-        <div className="form-group row">
-            <label htmlFor="inputBloodPressure" className="col-sm-12 col-form-label">Blood Pressure</label>
+          </div>
+          <div className="form-group row">
+            <label
+              htmlFor="inputBloodPressure"
+              className="col-sm-12 col-form-label"
+            >
+              Blood Pressure
+            </label>
             <div className="col-sm-6">
-                <label htmlFor="inputBloodPressure" className="col-sm-6 col-form-label">Systolic</label>
-                <input type="number" className="form-control" id="inputBloodPressure" placeholder="mm Hg(higher)"></input>
+              <label
+                htmlFor="inputBloodPressure"
+                className="col-sm-6 col-form-label"
+              >
+                Systolic
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="inputBloodPressure"
+                placeholder="mm Hg(higher)"
+                value={systolic}
+                onChange={e => onFormChange(e)}
+              ></input>
             </div>
             <div className="col-sm-6">
-            <label htmlFor="inputBloodPressure" className="col-sm-6 col-form-label">Diastolic</label>
-            <input type="number" className="form-control" id="inputBloodPressure" placeholder="mm Hg(lower)"></input>
+              <label
+                htmlFor="inputBloodPressure"
+                className="col-sm-6 col-form-label"
+              >
+                Diastolic
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="inputBloodPressure"
+                placeholder="mm Hg(lower)"
+                value={diastolic}
+                onChange={e => onFormChange(e)}
+              ></input>
             </div>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
-    
-        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default HealthForms;

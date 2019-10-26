38d4ab62d4a272563c04c4ef908fc5813e1efc31
Alvin Balmeo <albalmeo@gmail.com>
2019-10-26 13:31:26 -0700

    Testing sidebar styling

--- client/src/components/pages/JournalEntries.js
+++ client/src/components/pages/JournalEntries.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import SideNav, {
-  Toggle,
-  Nav,
-  NavItem,
-  NavIcon,
-  NavText
-} from '@trendmicro/react-sidenav';
-import JournalEntry from './JournalEntry';
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import React, { useState } from 'react';
 
 const JournalEntries = entries => {
-  // entries.map(entry => {});
+  const [sideBar, setSideBar] = useState({
+    width: 0,
+    marginLeft: 0
+  });
+
+  const { width, marginLeft } = sideBar;
+
+  const toggleSideBar = e => {
+    e.preventDefault();
+    if (width === 0 && marginLeft === 0) {
+      setSideBar({ ...sideBar, width: 250, marginLeft: 250 });
+    } else if (width === 250 && marginLeft === 250) {
+      setSideBar({ ...sideBar, width: 0, marginLeft: 0 });
+    }
+
+    console.log(sideBar);
+  };
+
   return (
     <>
-      <div id="mySidebar" className="sidebar">
-        <a href="javascript:void(0)" className="closebtn" onclick="closeNav()">
-          &times;
-        </a>
+      <div id="mySidebar" style={sideBar} className="sidebar">
+        <button className="closebtn" onClick="closeNav()"></button>
         <button className="btn" href="#">
           About
         </button>
@@ -31,8 +37,8 @@ const JournalEntries = entries => {
         </button>
       </div>
 
-      <div id="main">
-        <button class="openbtn" onclick="openNav()">
+      <div className="main" style={sideBar}>
+        <button className="openbtn" onClick={e => toggleSideBar(e)}>
           &#9776; Toggle Sidebar
         </button>
         <h2>Collapsed Sidebar</h2>

d7280480c467b240dd63c7da20df59e99fb874c0
Alvin Balmeo <albalmeo@gmail.com>
2019-11-01 20:14:50 -0700

    Adding css for loading and bloodPressure draft for taking in properties and creating graphs

--- client/src/components/graphs/BloodPressure.js
+++ client/src/components/graphs/BloodPressure.js
@@ -3,55 +3,11 @@ import { obtainBPData } from './graphUtils';
 import { Line } from 'react-chartjs-2';
 import 'chartjs-plugin-annotation';
 
-const BloodPressureChart = () => {
-  // TODO: update with useEffect and real data once functionality is finished; mockdata for now
-  const [bloodPressureValues, setBloodPressureValues] = useState({
-    bloodPressure: [
-      {
-        date: '12/1/2018',
-        systolic: 120,
-        diastolic: 90
-      },
-      {
-        date: '12/1/2018',
-        systolic: 120,
-        diastolic: 80
-      },
-      {
-        date: '12/2/2018',
-        systolic: 160,
-        diastolic: 90
-      },
-      {
-        date: '12/3/2018',
-        systolic: 120,
-        diastolic: 80
-      },
-      {
-        date: '12/4/2018',
-        systolic: 135,
-        diastolic: 95
-      },
-      {
-        date: '12/5/2018',
-        systolic: 120,
-        diastolic: 80
-      },
-      {
-        date: '12/6/2018',
-        systolic: 120,
-        diastolic: 80
-      },
-      {
-        date: '12/7/2018',
-        systolic: 120,
-        diastolic: 80
-      }
-    ]
-  });
+const BloodPressureChart = bloodPressure => {
+  console.log('bloodPressure', bloodPressure);
 
-  const { bloodPressure } = bloodPressureValues;
   let dataPoints = obtainBPData(bloodPressure);
+  console.log('dataPoints', dataPoints);
 
   return (
     <div className="chart">
